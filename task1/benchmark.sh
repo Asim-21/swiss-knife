52434a04857275af3fda2b88829f7b3c955e6653
teamf <ge58yeq@mytum.de>
2021-11-03 21:02:43 +0000

    add folder teamf_basic and teamf_exploration

--- task1/benchmark.sh
+++ task1/benchmark.sh
@@ -2,5 +2,5 @@
 mkdir -p temp
 for i in {10..100..10}; do
 	echo ${i}
-	docker run --rm --net=host ghcr.io/six-ddc/plow http://ryan.dse.in.tum.de:8091/swissknife-Teamf/ -c 20 -n 100000 -d 10s -i 0 | grep RPS  > temp/connection_${i}
+	docker run --rm --net=host ghcr.io/six-ddc/plow http://ryan.dse.in.tum.de:8094/swissknife-Teamf/ -c 20 -n 100000 -d 10s -i 0 | grep RPS  > temp/connection_${i}
 done
