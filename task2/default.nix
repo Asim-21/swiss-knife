a5666863d23073a514945b7c4ff447edb133a5b5
teamf <ge58yeq@mytum.de>
2021-11-19 02:02:08 +0000

    setup task2

---
+++ task2/default.nix
@@ -0,0 +1,7 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c1981eb51ecb109ddf23e1ff38e740626f62ec3e.tar.gz") {} }:
+
+pkgs.mkShell {
+  nativeBuildInputs = [
+    pkgs.python3.pkgs.seaborn
+  ];
+}
---
+++ task2/server_basic.py
@@ -0,0 +1,26 @@
+from socket import (
+    socket, 
+    AF_INET, 
+    SOCK_STREAM,
+    SO_REUSEADDR,
+    SOL_SOCKET
+)
+
+HOST, PORT = "127.0.0.1", 8080
+response =  b"HTTP/1.1 200 OK\n\nHello World\n"
+
+with socket(AF_INET, SOCK_STREAM) as sock:
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    sock.bind((HOST, PORT))
+    sock.listen(1)
+    while True:
+        try:
+            conn, addr = sock.accept()
+            req = conn.recv(1024).decode()
+            print(req)
+            conn.sendall(response)
+            conn.close()
+        except KeyboardInterrupt:
+            exit(0)
+        except Exception as e:
+            print(e)
