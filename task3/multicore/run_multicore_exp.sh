29e3a558c41048eca91c541be63ac3e220cb4369
teamf <ge58yeq@mytum.de>
2022-01-14 00:45:57 +0000

    use rclone to mount google drive

--- task3/multicore/run_multicore_exp.sh
+++ task3/multicore/run_multicore_exp.sh
@@ -2,7 +2,7 @@ mkdir -p temp
 cd temp
 TEMP_DIR=`pwd`
 
-cp /home/teamf/temp_share/parsec-3.0.tar.gz .
+nix-shell -p rclone --run 'rclone copy gg:swiss/parsec-3.0.tar.gz .'
 tar -xvf parsec-3.0.tar.gz
 
 wget http://parsec.cs.princeton.edu/download/3.0/parsec-3.0-input-sim.tar.gz
