b3aa5b907a5a272485731d84be169c41be7293b5
Asim-21 <76809539+Asim-21@users.noreply.github.com>
2022-01-18 08:34:40 +0100

    Update filesystem.sh

--- task3/filesystem/filesystem.sh
+++ task3/filesystem/filesystem.sh
@@ -1,3 +1,8 @@
+. ./variables
+sudo rm -rf ${mount_ext4}
+sudo rm -rf ${btrfs}
+mkdir ${mount_ext4}
+mkdir ${btrfs}
 echo '<--------------------> Starting partitioning and creating ext4 and btrfs fs <-------------------->'
 . ./format.sh
 echo '<--------------------> Running fio on ext4 and btrfs fs <-------------------->'
