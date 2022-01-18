673756dcda5831a3705e09682852c3ef759a53c0
Asim-21 <76809539+Asim-21@users.noreply.github.com>
2022-01-18 08:20:21 +0100

    Update wipe.sh

--- task3/filesystem/wipe.sh
+++ task3/filesystem/wipe.sh
@@ -5,8 +5,8 @@ sudo umount ${fs}2
 sudo umount ${fs}1
 sudo wipefs --all ${fs}1
 sudo wipefs --all ${fs}2
-echo -e 'd\n\nw\n' | sudo fdisk -c ${fs}
-echo -e 'd\n\nw\n' | sudo fdisk -c ${fs}
+#echo -e 'd\n\nw\n' | sudo fdisk -c ${fs}
+#echo -e 'd\n\nw\n' | sudo fdisk -c ${fs}
 sleep 5s
 sudo partprobe ${fs}
 
