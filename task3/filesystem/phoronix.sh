dd952c29b5f1c86825cedf71f51bd7040d392239
Asim-21 <76809539+Asim-21@users.noreply.github.com>
2022-01-18 08:27:56 +0100

    Update phoronix.sh

--- task3/filesystem/phoronix.sh
+++ task3/filesystem/phoronix.sh
@@ -2,7 +2,7 @@ echo "<---------------------------> Enter results filename here (v1 v2 etc.) <--
 read m
 . ./variables.sh
 docker run --name pts \
-    -v ~/swiss-knife/task3/filesystem/${mount_ext4}:/var/lib/phoronix-test-suite \
+    -v $(pwd)/${mount_ext4}:/var/lib/phoronix-test-suite \
     -d phoronix/pts
 docker exec -it pts /bin/bash \
     -c "cd phoronix-test-suite/;./phoronix-test-suite install disk;yes | ./phoronix-test-suite run disk SKIP_TESTS=pts/compilebench-1.0.3,pts/dbench-1.0.2,pts/ior-1.1.1,pts/iozone-1.9.6,pts/postmark-1.1.2"
